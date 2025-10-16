b9c7de5a157897a5037e2cf66842b21edc68e238
npolyak <nick.polyak@verizon.net>
2025-10-16 13:25:55 -0400

    C#

---
+++ AirdropTest/C#/AirdropTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using Solnet.Rpc;
+using Solnet.Wallet;
+
+namespace AirdropTest;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        IRpcClient client = ClientFactory.GetClient(Cluster.DevNet);
+
+        var acct = await client
+            .GetAccountInfoAsync(new PublicKey("A2GKaAD5FZ2TfaRq55mgJht9R9e39SjitaHCKxcYPs46"));
+
+        var requestResult = 
+            await client.RequestAirdropAsync
+            (
+                "A2GKaAD5FZ2TfaRq55mgJht9R9e39SjitaHCKxcYPs46", 
+                100_000_000);
+
+        Console.WriteLine("Hello, World!");
+    }
+}
