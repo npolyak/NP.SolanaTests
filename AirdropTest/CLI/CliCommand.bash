42ae1c9c5f2f0666a2db47df5fe15b232cfa875c
npolyak <nick.polyak@verizon.net>
2025-10-16 14:15:16 -0400

    added cli commands

---
+++ AirdropTest/CLI/CliCommand.bash
@@ -0,0 +1,8 @@
+rustc --version && solana --version && anchor --version && node --version && yarn --version
+
+# print the account info
+solana account A2GKaAD5FZ2TfaRq55mgJht9R9e39SjitaHCKxcYPs46 -u devnet
+
+# airdrop some sol to the account
+solana airdrop 0.000001 A2GKaAD5FZ2TfaRq55mgJht9R9e39SjitaHCKxcYPs46 -u devnet
+
